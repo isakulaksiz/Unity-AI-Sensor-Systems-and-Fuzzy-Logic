0540b825ad4afdb9c7efde9d01f1719b0fa8bbe2
isa <isakulaksiz@outlook.com>
2020-03-25 17:00:06 +0300

    Update ChaseState.cs

--- Assets/Scripts/ChaseState.cs
+++ Assets/Scripts/ChaseState.cs
@@ -18,9 +18,7 @@ public class ChaseState : StateMachineBehaviour
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+   
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
